9d503e78a7867c235f4edc0e1aa0eec384f754f0
adimerpaul <adimer101@email.com>
2020-05-17 21:58:53 -0400

    dev4

--- src/index.js
+++ src/index.js
@@ -1,40 +1,37 @@
+// const app = require('./app');
+// const http = require('http');
+// const socketio = require('socket.io');
+//
+// const server = http.createServer(app);
+// const io = socketio.listen(server);
+// require('./sockets')(io);
+// require('./database');
+//
+// async function main() {
+//   await server.listen(app.get('port'));
+//   console.log(`server on port ${app.get('port')}`);
+// }
+//
+// main();
+var cors = require('cors')
 var express=require('express');
 var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
-var Pedido=require('./models/Pedido');
+var path=require('path');
 require('./database');
-app.set('PORT',process.env.port || 3000);
+app.set('port', process.env.PORT || 3000);
+app.use(cors());
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+// app.use(express.static('public'));
 app.use(express.static(__dirname + '/public'));
-app.get('/', (req, res) => {
-    // res.sendFile(__dirname + '/admin.html');
-    res.send('aaa');
+
+app.get('/', function(req, res){
+  res.send('a');
 });
-//
-// io.on('connection', async (socket) => {
-//     let messages = await Pedido.find({estado:'ACTIVO'}).sort('-created');
-//     socket.emit('load old msgs', messages);
-//
-//     socket.on('pedido', async function(dat){
-//         var newPedido = new Pedido({
-//             lat: dat.lat,
-//             lng: dat.lng,
-//             name: dat.name,
-//             estado:'ACTIVO'
-//         });
-//         await newPedido.save();
-//         let messages = await Pedido.find({estado:'ACTIVO'}).sort('-created');
-//         io.emit('Places', messages);
-//     });
-//     socket.on('eliminar',async (dat)=> {
-//         await  Pedido.updateOne({_id:dat.id}, {estado:"INACTIVO"});
-//         let messages = await Pedido.find({estado:'ACTIVO'}).sort('-created');
-//         io.emit('Places', messages);
-//     });
-//     socket.on('chat message', (msg) => {
-//         io.emit('chat message', msg);
-//     });
-// });
-http.listen(app.get('PORT'), () => {
-    console.log('listening on *:'+app.get('PORT'));
+
+
+http.listen(app.get('port'), function(){
+    console.log('listening on *:'+app.get('port'));
 });
